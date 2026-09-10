5f1ad95d2c5feb3c70d1a197b73e15e8d69fc2ca
nfemhormuang <e54141804@gmail.com>
2026-09-10 14:57:19 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -2,6 +2,6 @@
   'use strict';
 
   window.APP_CONFIG = Object.freeze({
-    API_URL: ''
+    API_URL: 'https://script.google.com/macros/s/AKfycby8C1Sv420j_J05M2DzmDbvdU4KrSm2F5m4jjTm80d0W1_sAzwT5CX7H3uT7coivyRwUA/exec'
   });
 })();
