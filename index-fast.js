400309e667f9617d7420171511b026bca0ab56dc
nfemhormuang <e54141804@gmail.com>
2026-09-10 14:59:37 +0700

    Update index-fast.js

--- index-fast.js
+++ index-fast.js
@@ -1513,7 +1513,7 @@ if(document.readyState==='loading')document.addEventListener('DOMContentLoaded',
 
 /* ===== profile-config.js ===== */
 window.STUDENT_PROFILE_WEB_APP_URL =
-  '';
+  'https://script.google.com/macros/s/AKfycbwTedL-5qa6JpnAFbLVS2bvqFn9yDdQ9ZDZQ_vvKesliSXzxqDrbKI6oGPjOF2OvwBibg/exec';
 
 ;
 
